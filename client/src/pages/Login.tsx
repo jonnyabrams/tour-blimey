93bc34f9ea204ae32c7e3dd25716c0ff4dd25c31
jonnyabrams <jonnyabrams@hotmail.com>
2022-10-25 09:44:49 +0100

    feat: design Login page

--- client/src/pages/Login.tsx
+++ client/src/pages/Login.tsx
@@ -1,8 +1,84 @@
+import { useState, useEffect } from "react";
+import {
+  MDBCard,
+  MDBCardBody,
+  MDBInput,
+  MDBCardFooter,
+  MDBValidation,
+  MDBBtn,
+  MDBIcon,
+  MDBSpinner,
+} from "mdb-react-ui-kit";
+import { Link } from "react-router-dom";
+
+const initialState = {
+  email: "",
+  password: "",
+};
 
 const Login = () => {
-  return (
-    <div>Login</div>
-  )
-}
+  const [formValue, setFormValue] = useState(initialState);
+  const { email, password } = formValue;
 
-export default Login
+  const handleSubmit = (e: any) => {
+    e.preventDefault();
+  };
+
+  const onInputChange = (e: any) => {
+    let { name, value } = e.target;
+    setFormValue({ ...formValue, [name]: value });
+  };
+
+  return (
+    <div
+      style={{
+        margin: "auto",
+        padding: "15px",
+        maxWidth: "450px",
+        alignContent: "center",
+        marginTop: "120px",
+      }}
+    >
+      <MDBCard alignment="center">
+        <MDBIcon fas icon="user-circle" className="fa-2x" />
+        <h5>Sign In</h5>
+        <MDBCardBody>
+          <MDBValidation onSubmit={handleSubmit} noValidate className="row g-3">
+            <div className="col-md-12">
+              <MDBInput
+                placeholder="Email"
+                type="email"
+                value={email}
+                name="email"
+                onChange={onInputChange}
+                required
+              />
+            </div>
+            <div className="col-md-12">
+              <MDBInput
+                placeholder="Password"
+                type="password"
+                value={password}
+                name="password"
+                onChange={onInputChange}
+                required
+              />
+            </div>
+            <div className="col-12">
+              <MDBBtn style={{ width: "100%" }} className="mt-2">
+                Log In
+              </MDBBtn>
+            </div>
+          </MDBValidation>
+        </MDBCardBody>
+        <MDBCardFooter>
+          <Link to="/register">
+            <p>Don't have an account? Sign up here!</p>
+          </Link>
+        </MDBCardFooter>
+      </MDBCard>
+    </div>
+  );
+};
+
+export default Login;
