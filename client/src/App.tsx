9463c279f204cbfb7fa8cd646f55aec6befd81cd
jonnyabrams <jonnyabrams@hotmail.com>
2022-10-25 09:09:15 +0100

    chore: arrange routes

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,13 +1,24 @@
-import "./App.css";
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+
+import "./App.css";
+import Home from "./pages/Home";
+import Register from "./pages/Register";
+import Login from "./pages/Login";
 
 const App = () => {
   return (
-    <div className="App">
-      <ToastContainer />
-      <h1>Oi oi</h1>
-    </div>
+    <Router>
+      <div className="App">
+        <ToastContainer />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/register" element={<Register />} />
+          <Route path="/login" element={<Login />} />
+        </Routes>
+      </div>
+    </Router>
   );
 };
 
