e4e9e42d3fd157731d2983f6bd620cc58b979bb8
jonnyabrams <jonnyabrams@hotmail.com>
2022-10-25 09:06:02 +0100

    chore: client-side folders + configure toastify

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,8 +1,14 @@
-import React from "react";
 import "./App.css";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
 const App = () => {
-  return <div className="App"><h1>Oi oi</h1></div>;
-}
+  return (
+    <div className="App">
+      <ToastContainer />
+      <h1>Oi oi</h1>
+    </div>
+  );
+};
 
 export default App;
