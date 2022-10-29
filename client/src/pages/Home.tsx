c73ab364e0c07a58c285fbef1ae9947e6fe06011
jonnyabrams <jonnyabrams@hotmail.com>
2022-10-29 08:47:41 +0100

    feat: fetch all tours in home page

--- client/src/pages/Home.tsx
+++ client/src/pages/Home.tsx
@@ -1,9 +1,51 @@
+import { MDBCol, MDBContainer, MDBRow, MDBTypography } from "mdb-react-ui-kit";
+import { useEffect } from "react";
+import { Link } from "react-router-dom";
+import { TourType } from "../../typings/typings";
+import TourCard from "../components/TourCard";
 
+import { useAppDispatch, useAppSelector } from "../hooks";
+import { getAllTours } from "../redux/features/tourSlice";
 
 const Home = () => {
-  return (
-    <div>Home</div>
-  )
-}
+  const { tours, loading } = useAppSelector((state) => state.tour);
+  const dispatch = useAppDispatch();
 
-export default Home
+  // trigger getAllTours in tourSlice which populates state.tours, extracted above
+  useEffect(() => {
+    dispatch(getAllTours());
+  }, []);
+
+  if (loading) {
+    return <h2>Loading...</h2>;
+  }
+
+  return (
+    <div
+      style={{
+        margin: "auto",
+        padding: "15px",
+        maxWidth: "1000px",
+        alignContent: "center",
+      }}
+    >
+      <MDBRow className="mt-5">
+        {tours.length === 0 && (
+          <MDBTypography className="text-center mb-0" tag="h2">
+            No tours found
+          </MDBTypography>
+        )}
+        <MDBCol>
+          <MDBContainer>
+            <MDBRow className="row-cols-1 row-cols-md-3 g-2">
+              {tours &&
+                tours.map((item: TourType, index: number) => <TourCard />)}
+            </MDBRow>
+          </MDBContainer>
+        </MDBCol>
+      </MDBRow>
+    </div>
+  );
+};
+
+export default Home;
