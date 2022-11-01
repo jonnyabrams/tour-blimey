194b939d96757db9b7362eb7132514d835ae7602
jonnyabrams <jonnyabrams@hotmail.com>
2022-11-01 12:06:23 +0000

    feat: like button on each TourCard

--- client/src/components/TourCard.tsx
+++ client/src/components/TourCard.tsx
@@ -5,12 +5,27 @@ import {
   MDBCardText,
   MDBCardImage,
   MDBCardGroup,
+  MDBBtn,
+  MDBIcon,
 } from "mdb-react-ui-kit";
 import { Link } from "react-router-dom";
 import { TourType } from "../../typings/typings";
 import { excerpt } from "../utils/helpers";
 
 const TourCard = ({ tour }: { tour: TourType }) => {
+  const Likes = () => {
+    return (
+      <>
+        <MDBIcon far icon="thumbs-up" />
+        &nbsp;Like
+      </>
+    )
+  }
+
+  const handleLike = () => {
+
+  }
+
   return (
     <MDBCardGroup>
       <MDBCard className="h-100 mt-2 d-sm-flex" style={{ maxWidth: "20rem" }}>
@@ -26,8 +41,11 @@ const TourCard = ({ tour }: { tour: TourType }) => {
         <div className="top-left">{tour.name}</div>
         <span className="text-start tag-card">
           {tour.tags.map((tag) => (
-            <Link key={tag} to={`/tours/tag/${tag}`}>#{tag} </Link>
+            <Link key={tag} to={`/tours/tag/${tag}`}>
+              #{tag}{" "}
+            </Link>
           ))}
+          <MDBBtn style={{ float: "right" }} tag="a" color="none" onClick={handleLike}><Likes /></MDBBtn>
         </span>
         <MDBCardBody>
           <MDBCardTitle className="text-start">{tour.title}</MDBCardTitle>
